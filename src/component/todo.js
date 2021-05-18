c6a26e3d313a1295198f464241b0bde64cdb4b40
suraj kumar <suraj.kumar@medfin.in>
2021-05-18 20:23:27 +0530

    added edit

--- src/component/todo.js
+++ src/component/todo.js
@@ -7,27 +7,54 @@ const Todo = () => {
 
     const [inputData,setInputData]=useState("");
     const [items,setItems]=useState([]);
+    const [toggleSubmit,setToggleSubmit]=useState(true);
+    const [isEditItem,setIsEditItem]=useState(null);
 
     const addItem=()=>{
         if(!inputData){
 
-        }else{
-
-        setItems([...items,inputData]);
+        }else if(inputData && !toggleSubmit){
+            setItems(
+                items.map((value)=>{
+                    if(value.id===isEditItem){
+                        return {...value,name:inputData}
+                    } 
+                    return value
+                })
+            )
+            setToggleSubmit(true);
+            setInputData('');
+            setIsEditItem(null)
+        }       
+        else{
+            const allInputData={id:new Date().getTime().toString(),name:inputData}
+        setItems([...items,allInputData]);
         setInputData("")
     }
     } 
 
     //Delete the item
-    const deleteItem=(id)=>{
-        const updatedData=items.filter((value,index)=>{
-            return index!==id;
+    const deleteItem=(index)=>{
+        const updatedData=items.filter((value)=>{
+            return index!==value.id;
         })
         setItems(updatedData);
     }
     const removeAll=()=>{
         setItems([]);
     }
+
+    //Edit Item
+    const editItem=(id)=>{
+        let newEditItem=items.find((v)=>{
+            return v.id ===id
+        })
+        setToggleSubmit(false);
+        setInputData(newEditItem.name);
+        setIsEditItem(id)
+
+    }
+    
 
     return (
         <>
@@ -41,15 +68,23 @@ const Todo = () => {
                     <input type="text" placeholder="Add items" onChange={(e)=>{
                         setInputData(e.target.value)
                     }} value={inputData} />
-                    <i className="fa fa-plus add-btn" onClick={addItem} title="Add Item"></i>
-                </div>
+                    {
+                        toggleSubmit ? 
+                        <i className="fa fa-plus add-btn" onClick={addItem} title="Add Item"></i>: <i className="far fa-edit add-btn" onClick={addItem} title="Edit Item"></i>
+               
+             
+                    }
+                    </div>
 
                 <div className="showItems">
-                    {items.map((value,index)=>{
+                    {items.map((value)=>{
                         return(
-                            <div className="eachItem" key={index}>
-                            <h3>{value}</h3>
-                            <i className="far fa-trash-alt add-btn" title="Delete Item" onClick={()=>deleteItem(index)}></i>
+                            <div className="eachItem" key={value.id}>
+                            <h3>{value.name}</h3>
+                            <div className="todo-btn">
+                            <i className="far fa-edit add-btn" title="Edit Item" onClick={()=>editItem(value.id)}></i>
+                            <i className="far fa-trash-alt add-btn" title="Delete Item" onClick={()=>deleteItem(value.id)}></i>
+                            </div>
                         </div>
                         )
                     })}
